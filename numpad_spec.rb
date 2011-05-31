90c6dddfd51fdb2993da73c4ed8190a2eaa3e12e
Egbert Veenstra <egbertveenstra@gmail.com>
2011-05-31 18:09:59 +0200

    removed stupid test

--- numpad_spec.rb
+++ numpad_spec.rb
@@ -8,13 +8,4 @@ describe Numpad do
     end
     numpad.read_display.should == nil
   end
-  
-  it "accepts key presses when turned on" do
-    numpad = Numpad.new
-    numpad.turn_on
-    5.times do
-      numpad.press(9)
-    end
-    numpad.read_display.should == '99999'
-  end
 end
